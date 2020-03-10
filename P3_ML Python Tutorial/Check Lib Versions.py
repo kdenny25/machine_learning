6dfabe542a62d6ac279d0e70c621cdd4f805ce5b
kdenny25 <kelsead25@gmail.com>
2020-03-10 07:02:55 -0400

    Started Machine Learning python tutorial from Machinelarningmastery.com

---
+++ P3_ML Python Tutorial/Check Lib Versions.py
@@ -0,0 +1,25 @@
+# check the versions of libraries
+
+#Python Version
+import sys
+print('Python: {}'.format(sys.version))
+
+# scipy
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+
+# numpy
+import numpy
+print('numpy: {}'.format(numpy.__version__))
+
+# matplotlib
+import matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+
+# pandas
+import pandas
+print('pandas: {}'.format(matplotlib.__version__))
+
+# scikit-learn
+import sklearn
+print('sklearn: {}'.format(sklearn.__version__))
---
+++ P3_ML Python Tutorial/ML Python Tutorial.py
@@ -0,0 +1,61 @@
+# Load Libraries
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# Load dataset from UCI Machine Learning Repository
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+
+#Summarize the Dataset
+
+#Dimensions
+print(dataset.shape)
+
+#head
+print(dataset.head(20))
+
+#Statistical Summary
+print(dataset.describe())
+
+#Class distribution
+print(dataset.groupby('class').size())
+
+#Univariate Plots
+
+#Box and whisker plots
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+pyplot.show()
+
+#histograms
+dataset.hist()
+pyplot.show()
+
+#Multivariate Plots
+
+# scatter plot matrix
+scatter_matrix(dataset)
+pyplot.show()
+
+# Create a Validation dataset
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:4]
+y = array[:,4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+
+
